2ae2dc51aa36e6df8dbd9aee1bc6f5a9292a2bde
Guillaume Seimandi <36552311+iSeaox@users.noreply.github.com>
2022-03-04 11:13:24 +0100

    adding a running sprite in console

--- pixel_test.py
+++ pixel_test.py
@@ -1,8 +1,55 @@
 import sys
 import time
+import keyboard
 
+running = ["""
+      ██
+    ██████
+  ██  ██  ████
+      ██
+      ██
+    ██  ██
+████      ██
+            ██
+""", """
+      ██
+    ██████
+  ██  ██  ████
+      ██
+      ██
+    ██  ██
+████      ██
+          ██
+""", """
+      ██
+    ██████
+  ██  ██  ████
+  ██  ██
+      ██
+      ████
+    ██████
+        ██
+""", """
+      ██
+    ██████
+  ██  ██  ████
+  ██  ██
+      ██
+    ██  ████
+  ██        ██
+  ██
+""", """
+      ██
+    ██████
+  ██  ██  ████
+  ██  ██
+      ██
+    ██  ████
+████        ██
+
+"""]
 width = 120
-height = 20
+height = 40
 canvas = []
 
 for i in range(height):
@@ -31,6 +78,26 @@ def build(tab):
 
     return temp
 
+def blit(can, sprite, x, y):
+    splited_sprite = sprite.split("\n")
+    for i in range(len(splited_sprite)):
+        j = 0
+        while(j < len(splited_sprite[i])):
+            if(splited_sprite[i][j:(j+2)] == "██"):
+                canvas[y + i][x + (j // 2)] = 1
+            j += 2
+
+
+global x
+global y
+x = 10
+y = 10
+
+def change():
+    global y
+    y -= 8
+
+keyboard.add_hotkey('esc', lambda: change())
 
 index = 0
 while(1):
@@ -38,17 +105,21 @@ while(1):
     # while(count >= 0):
     #     sys.stdout.write("\n")
     #     count -= 1
+
     canvas = []
     for i in range(height):
         temp = []
         for k in range(width):
             temp.append(0)
         canvas.append(temp)
-    canvas[5][index] = 1
 
+    if(index % len(running) == 0):
+        y = 10
+
+    blit(canvas, running[index % len(running)], x, y)
     to_print = build(canvas)
 
     for line in to_print:
-        print(line)
-    time.sleep(0.05)
+        print(line, end="")
+    time.sleep(0.2)
     index += 1
